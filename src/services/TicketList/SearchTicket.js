68462ca7f825b544b8f303f1b177f8174d46d57f
Sofie <you@example.com>
2024-08-27 20:03:01 +0300

    fixes again

--- src/services/TicketList/SearchTicket.js
+++ src/services/TicketList/SearchTicket.js
@@ -1,4 +1,3 @@
-
 import { aviasalesHost } from "../aviasalesHost.js";
 import { listAction } from "../../store/reducers/TicketList.js";
 import { toast } from "react-toastify";
@@ -9,53 +8,51 @@ export const getTicketsApi = (id) => async (dispatch) => {
 
     while (!stop) {
         try {
-            const { data } = await aviasalesHost.get(`tickets?searchId=${id}`).catch((e) => {
-                if (e.response) {
-                    toast.error(`Ошибка сервера`, {
-                        position: "bottom-left",
-                        autoClose: 6000,
-                        hideProgressBar: true,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                    });
-                }
-            });
+            const { data } = await aviasalesHost.get(`tickets?searchId=${id}`);
 
-            if (data) {
-                // билеты к списку
-                allTickets = [...allTickets, ...data.tickets];
+            // Добавляем билеты к списку
+            allTickets = [...allTickets, ...data.tickets];
 
-                // Обновляем список Redux store
-                dispatch(listAction(allTickets));
+            // Обновляем список в Redux store
+            dispatch(listAction(allTickets));
 
-                
-                stop = data.stop;
-            }
+            // Проверяем, нужно ли завершить цикл
+            stop = data.stop;
 
         } catch (e) {
-            toast.error(`Ошибка при загрузке билетов`, {
-                position: "bottom-left",
-                autoClose: 6000,
-                hideProgressBar: true,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            });
-
-            
+            // cервер - игнор, иначе - показываем пользователю ошибку 
+            if (e.response && e.response.status >= 500 && e.response.status < 600) {
+                toast.error('Ошибка сервера', {
+                    position: "bottom-left",
+                    autoClose: 6000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            } else {
+                toast.error('Ошибка при загрузке билетов', {
+                    position: "bottom-left",
+                    autoClose: 6000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+                stop = true; // Прекращаем цикл не сервер ошибка
+            }
         }
     }
 };
 
 export const getSearchApi = () => async (dispatch) => {
     try {
-        const { data } = await aviasalesHost.get(`search`);
+        const { data } = await aviasalesHost.get('search');
         dispatch(getTicketsApi(data.searchId));
     } catch (e) {
-        toast.error(`Ошибка при инициализации поиска`, {
+        toast.error('Ошибка при инициализации поиска', {
             position: "bottom-left",
             autoClose: 6000,
             hideProgressBar: true,
